68826fa0715c1edd89db12632571ad4059226c4d
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-02-17 22:28:30 +0100

    update

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import Customer
 from .models import Product
 from .models import Category
+
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -1,36 +1,59 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
-class User(models.Model):
+class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=14)
     delivery_Address = models.TextField()
+    password = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f" {self.first_name} {self.last_name}"
+    
+
+
     
 class Category(models.Model):
     name = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.name}"
+        return self.name
     
     class Meta:
         verbose_name_plural = 'categories'
 
 
+
+
+
 class Product(models.Model):
     name = models.CharField(max_length=50)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)   
-    image = models.ImageField(upload_to='upload/media/')
+    image = models.ImageField(upload_to='upload/product/')
     description = models.TextField()
-    price = models.DecimalField(max_digit=2, decimal_places=2)
-    discount_price = models.DecimalField(max_digit=2,decimal_places=2)
+    price = models.DecimalField(default =0, max_digits=6, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=2,decimal_places=2)
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
+    
+
+
+    
+
+class Order(models.Model):
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    orderNumber = models.ImageField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    address = models.TextField()
+    phone = models.CharField(max_length=14)
+    status = models.BooleanField(default = False)
+    date = models.DateTimeField(default = datetime.today)
+
     
 
 
@@ -40,5 +63,7 @@ class vendor(models.Model):
     store_name = models.TextField()
     product_type = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return f"{self.vendor_full_name}"
 
 
