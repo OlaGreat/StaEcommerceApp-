33672ac1ab3710eb7464206d26ffa32cf6682e6c
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-02-18 05:43:29 +0100

    update

--- store/models.py
+++ store/models.py
@@ -34,8 +34,8 @@ class Product(models.Model):
     Category = models.ForeignKey(Category, on_delete=models.CASCADE)   
     image = models.ImageField(upload_to='upload/product/')
     description = models.TextField()
-    price = models.DecimalField(default =0, max_digits=6, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=2,decimal_places=2)
+    price = models.DecimalField(default =0, max_digits=5, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=5,decimal_places=2)
 
     def __str__(self):
         return self.name
