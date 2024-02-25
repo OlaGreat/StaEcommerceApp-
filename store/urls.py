653bbaca7e1c78fe941e0b8dd420fe0a5e963343
OlaGreat <oladipupoolamilekan2@gmail.com>
2024-02-25 21:53:53 +0100

    update

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',  views.home, name='homePage'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
+    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='login'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import login, logout
 
 # Create your views here.
 
 
 def home(request):
-
-    return render(request, 'home.html',{})
+    products = Product.objects.all()
+    return render(request, 'home.html',{'products' : products})
 
 
 def about(request):
     return render(request, 'about.html',{})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def login_user(request):
+    return redirect('home')
